5d4707baed6de0c6c92ec02c308c0e5116ffd189
Oleksandr <OlexandrTanasienko@gmail.com>
2022-12-02 19:13:33 +0200

    Change hostname

--- src/app.js
+++ src/app.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(`${path}index.html`);
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`);
 });
