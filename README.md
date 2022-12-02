b87335fd883f36f81c0bad5ce828a5f12d3e269f
Oleksandr <OlexandrTanasienko@gmail.com>
2022-12-02 15:37:03 +0200

    Setup eslint and prettier

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 # docker-task
-Dockerhub link: 
+
+Dockerhub link:
 https://hub.docker.com/repository/docker/erbidium/docker-task
 
 To build image
+
 ```
 docker build -t erbidium/docker-task
 ```
+
 To push image
+
 ```
 docker login
 docker push erbidium/docker-task
 ```
+
 To pull image
+
 ```
 docker pull erbidium/docker-task:latest
 ```
+
 To run image with memory and CPU limits on the port 80
+
 ```
 docker run -p 80:80 --cpus=1 -m 512m erbidium/docker-task
 ```
--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+
 const app = express();
 
-const path = __dirname + '/views/';
+const path = `${__dirname}/views/`;
 const port = 80;
 
 app.use(express.static(path));
 
-app.get('/', function(req,res){
-    res.sendFile(path + 'index.html');
-  });
+app.get('/', (req, res) => {
+    res.sendFile(`${path}index.html`);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
